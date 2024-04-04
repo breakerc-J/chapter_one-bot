fc7f0ba87b9cad285643bcd9ccb89c2a6d032d79
breakerc-J <60805250+breakerc-J@users.noreply.github.com>
2024-04-04 13:49:27 +0900

    'gpt_bot'

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -57,6 +57,7 @@ memory_llm = None
 if api_key:
     llm = ChatOpenAI(
         temperature=0.1,
+        api_key=api_key,
         streaming=True,
         callbacks=[
             ChatCallbackHandler(),
@@ -86,7 +87,7 @@ def embed_file(file):
     )
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
-    embeddings = OpenAIEmbeddings()
+    embeddings = OpenAIEmbeddings(api_key=api_key)
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
     retriever = vectorstore.as_retriever()
